4c6632c2f3dee8e06d6eb5bf39bfebb3da251d09
dicks6n <dikkymccria@gmail.com>
2019-08-11 02:14:05 -0700

    adding function index to display title

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,10 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'GraphersBlog'
