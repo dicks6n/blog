d6e2e0911d26f90bb5f3009754bf77722028cb08
dicks6n <dikkymccria@gmail.com>
2019-08-11 02:40:40 -0700

    Adding a filter that directs you to error 404 when you not a registered user

--- app/main/views.py
+++ app/main/views.py
@@ -28,3 +28,13 @@ def index():
         return redirect(url_for('main.index'))
     else:
         return render_template('index.html', title = title, blogs = allBlogs, quote = quote, comments = comments)
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    blogs = Blog.query.filter_by(user_id = user_id).order_by(Blog.posted.desc())
+
+    if user is None :
+        abort(404)
+
+    return render_template("profilr/profile.html" user = user ,blogs =blogs) 
+
