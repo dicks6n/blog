1fa12cc8edacd30c924009972403ee85cd50bebd
dicks6n <dikkymccria@gmail.com>
2019-08-11 03:02:42 -0700

    Adding a login an dlink to profile

--- app/main/views.py
+++ app/main/views.py
@@ -54,3 +54,15 @@ def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
     if user is None:
         abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form,user=user)   
