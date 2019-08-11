dcc470d238c4d06cc0bc7dd9c71ba812b07b8e0f
dicks6n <dikkymccria@gmail.com>
2019-08-11 02:19:42 -0700

    inporting models

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,12 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 from .forms import BlogForm,CommentForm, UpdateProfile
+from ..models import Comment, User, Blog,Member
+from .. import db, photos
+from ..request import get_quote
+from flask_login import login_required, current_user
+import markdown2
+from ..email import mail_message
 
 @main.route('/')
 def index():
